82e8594d682bf512d39b605859cadbd8d75fdea7
macc001 <miguelcayocarvajal37@gmail.com>
2021-12-25 21:03:14 -0400

    re estructuracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, VERSION } from '@angular/core';
-import { forkJoin, from, Observable } from 'rxjs';
-import { concatMap, mergeMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -9,95 +6,5 @@ import { concatMap, mergeMap } from 'rxjs/operators';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-  dataForkJoin: any[] = [];
-
-  constructor(private http: HttpClient) {
-    this.getDataForkJoin();
-    this.getDataConcatMap();
-    this.getDataaMergeMap();
-  }
-
-  getDataForkJoin() {
-    // let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // cambiar a -1 para que de error
-    let ids = [1, 2, 3]; // cambiar a -1 para que de error
-    let arrayOfData = [];
-    for (let i = 0; i < ids.length; i++) {
-      arrayOfData.push(this.getSomethingFromAnAPI(ids[i]));
-    }
-    forkJoin(arrayOfData).subscribe(
-      (response) => {
-        for (let item in Object.keys(response)) {
-          this.dataForkJoin.push({
-            body: response[item].body,
-            title: response[item].title,
-            id: response[item].id,
-          });
-        }
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  getSomethingFromAnAPI(id: number): any {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts/' + id);
-  }
-
-  dataConcatMap: any[] = [];
-  getDataConcatMap() {
-    let ids = [1, 2];
-    this.getSomethingFromAnAPI2(ids).subscribe(
-      (response) => {
-        this.dataConcatMap.push({
-          body: response.body,
-          title: response.title,
-          id: response.id,
-        });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  getSomethingFromAnAPI2(ids: number[]): any {
-    return from(ids).pipe(
-      concatMap(
-        (id) =>
-          <Observable<any>>(
-            this.http.get('https://jsonplaceholder.typicode.com/posts/' + id)
-          )
-      )
-    );
-  }
-
-  dataMergeMap: any[] = [];
-  getDataaMergeMap() {
-    let ids = [1, -2, 3, 4, 5, 6, 7, 8, 9, 10];
-    this.getSomethingFromAnAPI3(ids).subscribe(
-      (response) => {
-        this.dataMergeMap.push({
-          body: response.body,
-          title: response.title,
-          id: response.id,
-        });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  getSomethingFromAnAPI3(ids: number[]): any {
-    return from(ids).pipe(
-      mergeMap(
-        (id) =>
-          <Observable<any>>(
-            this.http.get('https://jsonplaceholder.typicode.com/posts/' + id)
-          )
-      )
-    );
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { SubjectComponent } from './00-subject/subject.component';
+import { ForkjoinMergemapConcatmapComponent } from './99_forkjoin-mergemap-concatmap/forkjoin-mergemap-concatmap.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [
+    AppComponent,
+    SubjectComponent,
+    ForkjoinMergemapConcatmapComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
